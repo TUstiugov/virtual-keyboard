2f78fc762ebab48f9235119b80b43bc2ce227374
T. Ustiugov <t.ustiugov@gmail.com>
2023-04-29 16:12:51 +0300

    feat: lock focus on textaria

--- src/components/text-area/text-area.js
+++ src/components/text-area/text-area.js
@@ -8,6 +8,9 @@ export default function createTextArea() {
   const textArea = document.createElement('textarea');
   textArea.classList.add(CssClasses.TEXT_AREA);
   textArea.rows = '10';
+  textArea.autofocus = true;
+
+  textArea.addEventListener('blur', () => textArea.focus());
 
   return textArea;
 }
